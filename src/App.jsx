8eb38fdde8c8bb8056343f071f9a0baa805cd089
coddingWorker-12 <kumarashok15957@gmail.com>
2025-06-17 06:29:59 +0530

    press shift + enter for show text in text area

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,31 @@ import './App.css'
 import ProgressBar from './ProgressBar'
 
 
-export default function App(){
-let arr = [1, 2, 3, 4];
-const [isFound, setIsFound] = useState(false);
-const isInArray = (array,value)=>{
- const item = array.find(item => item === value);
- setIsFound(!!item);
-}
-useEffect(()=>{
-  isInArray(arr,6);
-},[]);
-return(
-  <div>
-    {
-      isFound?<h1>true</h1>:<h1>false</h1>
-    }
-  </div>
-)
+export default function App(){ 
+  
+  const [messages, setMessages] = useState(["The minute she landed she understood the reason this was a fly-over state.", "She thought there'd be sufficient time if she hid her watch."]);
+  const [inputValue, setInputValue] = useState('');
 
+  const handelKeyDown = (e) => {
+   if(e.key === "Enter" && e.shiftKey){
+     e.preventDefault();
+     if(inputValue.trim() !== ""){
+      setMessages((prev)=>[...prev,inputValue]);
+      setInputValue('');
+     }
+   }
+  };
+
+
+  return(
+    <div>
+      {
+        messages.map((message,index)=>(
+          <div key={index}>{message}</div>
+        ))
+      }
+      <textarea type="text" onChange={(e)=>setInputValue(e.target.value)} onKeyDown={handelKeyDown} placeholder='after writing text press shift + enter for display' value={inputValue} />
+    </div>
+  )
+   
 }
