2479e83992388500b3a02f8817db44490602f971
coddingWorker-12 <kumarashok15957@gmail.com>
2025-06-22 07:29:06 +0530

    console object as initial

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,25 @@ import ProgressBar from './ProgressBar'
 
 
 export default function App(){ 
-  
-  const [messages, setMessages] = useState(["The minute she landed she understood the reason this was a fly-over state.", "She thought there'd be sufficient time if she hid her watch."]);
-  const [inputValue, setInputValue] = useState('');
 
-  const handelKeyDown = (e) => {
-   if(e.key === "Enter" && e.shiftKey){
-     e.preventDefault();
-     if(inputValue.trim() !== ""){
-      setMessages((prev)=>[...prev,inputValue]);
-      setInputValue('');
-     }
-   }
-  };
+   const user = {
+  firstName: 'John',
+ 
+  lastName: 'Doe'
+}
 
+const intialValue = (user)=>{
+  if(user.firstName && user.midelName && user.lastName){
+    console.log(user.firstName[0],user.middleName[0],user.lastName[0]);
 
-  return(
-    <div>
-      {
-        messages.map((message,index)=>(
-          <div key={index}>{message}</div>
-        ))
-      }
-      <textarea type="text" onChange={(e)=>setInputValue(e.target.value)} onKeyDown={handelKeyDown} placeholder='after writing text press shift + enter for display' value={inputValue} />
-    </div>
-  )
-   
+  }
+  else if(user.firstName && user.lastName){
+    console.log(user.firstName[0],user.lastName[0]);
+  }
+  else{
+    console.log(user.firstName[0])
+  }
+}
+
+intialValue(user);
 }
